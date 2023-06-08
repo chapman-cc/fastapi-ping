854f5a76d6354456ed304bd900e08df0a7195863
chapman-cheng <chapboy1989@gmail.com>
2023-06-08 16:21:20 +0800

    feat(src/__init__.py): add generate openapi schema

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
+import os
 
 from .routers.router import router
+import json
 
 tags = [
     {
@@ -18,20 +20,23 @@ app = FastAPI(openapi_tags=tags)
 
 app.include_router(router=router)
 
-def openapi_func ():
+def custom_openapi_func ():
     if (app.openapi_schema):
         return app.openapi_schema
     app.openapi_schema =  get_openapi(
         title="ping API",
         version="0.1.0",
-        description="This is a simple API to test the ping functionality",
         routes=app.routes,
+        description="This is a simple API to test the ping functionality",
         tags=tags,
-        openapi_version="3.0.0",
-)
+    )
+    return app.openapi_schema
 
-app.openapi = openapi_func
+app.openapi = custom_openapi_func
 
-@app.get("/echo", tags=["utils"])
-def echo():
-    return {"message": "Yean I can read you"}
+# write data to file
+if not os.path.isdir("./dist"): os.mkdir("./dist")
+with open("./dist/openapi.json", "w") as f:
+    openapi_schema = app.openapi()
+    f.write(json.dumps(openapi_schema))
+    f.close()
